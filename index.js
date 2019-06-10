1e77ef9015a8bbb71cb2fbdf707816880523ab25
unknown <tmartinez114@gmail.com>
2019-06-10 12:17:48 -0400

    created new member || utilizing uuid for id generator

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 const express = require("express");
 const path = require("path");
 const logger = require("./middleware/logger");
-const members = require("./Members");
 
 const app = express();
 
 // Init middleware
 // app.use(logger);
 
-// Gets All Members
-app.get("/api/members", (req, res) => {
-  res.json(members);
-});
-
-// Get Single Member
-app.get("/api/members/:id", (req, res) => {
-  const found = members.some(member => member.id === parseInt(req.params.id));
-
-  if (found) {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
-  }
-});
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Set a Static Folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// Members API Route
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
