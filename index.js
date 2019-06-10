4233b0b9d4ce720dff50fd4c8d31f81f807005ef
unknown <tmartinez114@gmail.com>
2019-06-09 20:48:48 -0400

    added members array to push as json

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ const path = require('path')
 
 const app = express();
 
+const members = [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'john@gmail.com',
+      status: 'active'
+    },
+    {
+      id: 2,
+      name: 'Bob Williams',
+      email: 'bob@gmail.com',
+      status: 'inactive'
+    },
+    {
+      id: 3,
+      name: 'Shannon Jackson',
+      email: 'shannon@gmail.com',
+      status: 'active'
+    }
+  ];
+
+app.get('/api/memebers', (req, res) => {
+    res.json(memebers);
+});
+
 // Set a Static Folder
 app.use(express.static(path.join(__dirname, 'public')))
 
