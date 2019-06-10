0ec084aa691317bf26b413e8a5710fe1cd8e0d36
unknown <tmartinez114@gmail.com>
2019-06-09 23:47:36 -0400

    getting single member and displaying msg if there isn't a member by a certain id

--- index.js
+++ index.js
@@ -6,11 +6,22 @@ const members = require("./Members");
 const app = express();
 
 // Init middleware
-app.use(logger);
+// app.use(logger);
 
 // Gets All Members
 app.get("/api/members", (req, res) => {
   res.json(members);
+});
+
+// Get Single Member
+app.get("/api/members/:id", (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
 });
 
 // Set a Static Folder
